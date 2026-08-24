6aa55f98fcb62b1415e94d34ad7b0c11fbf65d47
reehassan <theailaboratories@gmail.com>
2026-08-24 17:29:24 +0500

    domains: register admin and app config

---
+++ backend/domain_launch_assistant/domains/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class DomainsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "domain_launch_assistant.domains"
+    label = "domains"
+    
